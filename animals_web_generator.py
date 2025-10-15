3224db24b771d333e19c3391db0b2ecec898efb0
eStangel <e.stangel1986@gmail.com>
2025-10-15 17:28:16 +0200

    Added animal data to html file.

--- animals_web_generator.py
+++ animals_web_generator.py
@@ -5,16 +5,23 @@ def load_data(file_path):
   with open(file_path, "r") as handle:
     return json.load(handle)
 
-
 animals_data = load_data('animals_data.json')
-print(animals_data)
+output = ""
 for animal in animals_data:
-    print(
+    output += (
         f"Name: {animal['name']}\n"
         f"Diet: {animal['characteristics']['diet']}\n"
-        f"Location: {animal['locations'][0]}"
+        f"Location: {animal['locations'][0]}\n"
     )
     if animal['characteristics'].get('type', 0) != 0:
-        print(f"Type: {animal['characteristics']['type']}\n")
+        output += f"Type: {animal['characteristics']['type']}\n\n"
     else:
-        print()
+        output += "\n"
+
+with open("animals_template.html", "r") as f:
+    data = f.read()
+
+data = data.replace("__REPLACE_ANIMALS_INFO__", output)
+
+with open("animals.html", "w") as f:
+    f.write(data)
